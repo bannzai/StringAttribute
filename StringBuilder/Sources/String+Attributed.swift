6c96069702153294b108dce40d01cef01f8cf09d
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-14 22:01:26 +0900

    Add Sources

---
+++ StringBuilder/Sources/String+Attributed.swift
@@ -0,0 +1,15 @@
+//
+//  String+Attributed.swift
+//  StringAttribute
+//
+//  Created by Yudai.Hirose on 2017/07/14.
+//  Copyright © 2017年 Hirose.Yudai. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    public func attributed() -> NSAttributedString {
+        return NSAttributedString(string: self)
+    }
+}
