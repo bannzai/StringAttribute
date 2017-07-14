892dc068c185d0ed93fc1ef5755338f2b904bc09
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-14 22:10:39 +0900

    Remake xcodeproject

---
+++ StringAttribute/StringAttribute.h
@@ -0,0 +1,19 @@
+//
+//  StringAttribute.h
+//  StringAttribute
+//
+//  Created by Yudai.Hirose on 2017/07/14.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringAttribute.
+FOUNDATION_EXPORT double StringAttributeVersionNumber;
+
+//! Project version string for StringAttribute.
+FOUNDATION_EXPORT const unsigned char StringAttributeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringAttribute/PublicHeader.h>
+
+
