43dab35067de8f11cd4b695c7e50c11bb2281432
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-14 22:00:48 +0900

    Remake project

---
+++ StringBuilder/StringBuilder.h
@@ -0,0 +1,19 @@
+//
+//  StringBuilder.h
+//  StringBuilder
+//
+//  Created by Yudai.Hirose on 2017/07/14.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringBuilder.
+FOUNDATION_EXPORT double StringBuilderVersionNumber;
+
+//! Project version string for StringBuilder.
+FOUNDATION_EXPORT const unsigned char StringBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringBuilder/PublicHeader.h>
+
+
