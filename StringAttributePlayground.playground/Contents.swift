c53841c0a47f732e8a17f87af8a21f885e45b6eb
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-15 09:54:19 +0900

    Fix argumentname

--- StringAttributePlayground.playground/Contents.swift
+++ StringAttributePlayground.playground/Contents.swift
@@ -7,6 +7,6 @@ var str = "Hello, playground"
 
 "Hello, playground"
     .attributed()
-    .apply(for: .font(.boldSystemFont(ofSize: 15)))
-    .apply(for: .foregroundColor(.red))
+    .apply(with: .font(.boldSystemFont(ofSize: 15)))
+    .apply(with: .foregroundColor(.red))
 
