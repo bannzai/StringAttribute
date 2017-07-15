cd918a5ce5cbea298a24909ac6188170caa21fbc
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-15 09:54:03 +0900

    Add playgrounds

--- StringAttributePlayground.playground/Contents.swift
+++ StringAttributePlayground.playground/Contents.swift
@@ -5,3 +5,8 @@ import StringAttribute
 
 var str = "Hello, playground"
 
+"Hello, playground"
+    .attributed()
+    .apply(for: .font(.boldSystemFont(ofSize: 15)))
+    .apply(for: .foregroundColor(.red))
+
