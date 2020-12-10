011cf0ac16efc20919297a34dd9bcc2147c10681
bannzai <kingkonog999yhirose@gmail.com>
2020-12-10 12:11:45 +0900

    :recycle: Replace to apply(_:)

--- StringAttribute/Sources/AttributedStringBuilder.swift
+++ StringAttribute/Sources/AttributedStringBuilder.swift
@@ -22,55 +22,55 @@ public struct AttributedStringBuilder {
     
     
     @discardableResult
-    public func apply(with attribute: StringAttribute) -> AttributedStringBuilder {
+    public func apply(_ attribute: StringAttribute) -> AttributedStringBuilder {
         return apply(with: attribute, in: Range(uncheckedBounds: (lower: 0, upper: attributedString.length)))
     }
     
     @discardableResult
-    public func apply(with attributes: [StringAttribute]) -> AttributedStringBuilder {
+    public func apply(_ attributes: [StringAttribute]) -> AttributedStringBuilder {
         return apply(with: attributes, in: Range(uncheckedBounds: (lower: 0, upper: attributedString.length)))
     }
     
     @discardableResult
-    public func apply(with attribute: StringAttribute, in range: Range<Int>) -> AttributedStringBuilder {
+    public func apply(_ attribute: StringAttribute, in range: Range<Int>) -> AttributedStringBuilder {
         let nsRange = NSMakeRange(range.lowerBound, range.upperBound - range.lowerBound)
         attributedString.addAttributes(attribute.attributes, range: nsRange)
         return self
     }
     
     @discardableResult
-    public func apply(with attributes: [StringAttribute], in range: Range<Int>) -> AttributedStringBuilder {
+    public func apply(_ attributes: [StringAttribute], in range: Range<Int>) -> AttributedStringBuilder {
         let nsRange = NSMakeRange(range.lowerBound, range.upperBound - range.lowerBound)
         attributes.forEach { attributedString.addAttributes($0.attributes, range: nsRange) }
         return self
     }
     
     @discardableResult
-    public func apply(with attribute: StringAttribute, in countableRange: CountableRange<Int>) -> AttributedStringBuilder {
+    public func apply(_ attribute: StringAttribute, in countableRange: CountableRange<Int>) -> AttributedStringBuilder {
         let range: Range<Int> = Range(uncheckedBounds: (countableRange.lowerBound, countableRange.upperBound))
         return apply(with: attribute, in: range)
     }
     
     @discardableResult
-    public func apply(with attributes: [StringAttribute], in countableRange: CountableRange<Int>) -> AttributedStringBuilder {
+    public func apply(_ attributes: [StringAttribute], in countableRange: CountableRange<Int>) -> AttributedStringBuilder {
         let range: Range<Int> = Range(uncheckedBounds: (countableRange.lowerBound, countableRange.upperBound))
         return apply(with: attributes, in: range)
     }
     
     @discardableResult
-    public func apply(with attribute: StringAttribute, in closeRange: ClosedRange<Int>) -> AttributedStringBuilder {
+    public func apply(_ attribute: StringAttribute, in closeRange: ClosedRange<Int>) -> AttributedStringBuilder {
         let range: Range<Int> = Range(uncheckedBounds: (closeRange.lowerBound, closeRange.upperBound))
         return apply(with: attribute, in: range)
     }
     
     @discardableResult
-    public func apply(with attributes: [StringAttribute], in closeRange: ClosedRange<Int>) -> AttributedStringBuilder {
+    public func apply(_ attributes: [StringAttribute], in closeRange: ClosedRange<Int>) -> AttributedStringBuilder {
         let range: Range<Int> = Range(uncheckedBounds: (closeRange.lowerBound, closeRange.upperBound))
         return apply(with: attributes, in: range)
     }
     
     @discardableResult
-    public func apply(with attribute: StringAttribute, for string: String, to position: StringMatchPositionType) -> AttributedStringBuilder {
+    public func apply(_ attribute: StringAttribute, for string: String, to position: StringMatchPositionType) -> AttributedStringBuilder {
         position
             .convertRanges(
                 from: attributedString.string.ranges(of: string)
@@ -82,7 +82,7 @@ public struct AttributedStringBuilder {
     }
     
     @discardableResult
-    public func apply(with attributes: [StringAttribute], for string: String, to position: StringMatchPositionType) -> AttributedStringBuilder {
+    public func apply(_ attributes: [StringAttribute], for string: String, to position: StringMatchPositionType) -> AttributedStringBuilder {
         attributes.forEach {
             _ = apply(with: $0, for: string, to: position)
         }
--- StringAttribute/Sources/NSAttributedString+StringAttributeBuilder.swift
+++ StringAttribute/Sources/NSAttributedString+StringAttributeBuilder.swift
@@ -9,43 +9,43 @@
 import Foundation
 
 public extension NSAttributedString {
-    public func apply(with attribute: StringAttribute) -> NSAttributedString {
+    public func apply(_ attribute: StringAttribute) -> NSAttributedString {
         return apply(with: attribute, in: Range(uncheckedBounds: (lower: 0, upper: length)))
     }
     
-    public func apply(with attributes: [StringAttribute]) -> NSAttributedString {
+    public func apply(_ attributes: [StringAttribute]) -> NSAttributedString {
         return apply(with: attributes, in: Range(uncheckedBounds: (lower: 0, upper: length)))
     }
     
-    public func apply(with attribute: StringAttribute, in range: Range<Int>) -> NSAttributedString {
+    public func apply(_ attribute: StringAttribute, in range: Range<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attribute, in: range).build()
     }
     
-    public func apply(with attributes: [StringAttribute], in range: Range<Int>) -> NSAttributedString {
+    public func apply(_ attributes: [StringAttribute], in range: Range<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attributes, in: range).build()
     }
     
-    public func apply(with attribute: StringAttribute, in range: CountableRange<Int>) -> NSAttributedString {
+    public func apply(_ attribute: StringAttribute, in range: CountableRange<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attribute, in: range).build()
     }
     
-    public func apply(with attributes: [StringAttribute], in range: CountableRange<Int>) -> NSAttributedString {
+    public func apply(_ attributes: [StringAttribute], in range: CountableRange<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attributes, in: range).build()
     }
     
-    public func apply(with attribute: StringAttribute, in range: ClosedRange<Int>) -> NSAttributedString {
+    public func apply(_ attribute: StringAttribute, in range: ClosedRange<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attribute, in: range).build()
     }
     
-    public func apply(with attributes: [StringAttribute], in range: ClosedRange<Int>) -> NSAttributedString {
+    public func apply(_ attributes: [StringAttribute], in range: ClosedRange<Int>) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attributes, in: range).build()
     }
     
-    public func apply(with attribute: StringAttribute, for string: String, to position: StringMatchPositionType = .all) -> NSAttributedString {
+    public func apply(_ attribute: StringAttribute, for string: String, to position: StringMatchPositionType = .all) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attribute, for: string, to: position).build()
     }
     
-    public func apply(with attributes: [StringAttribute], for string: String, to position: StringMatchPositionType = .all) -> NSAttributedString {
+    public func apply(_ attributes: [StringAttribute], for string: String, to position: StringMatchPositionType = .all) -> NSAttributedString {
         return AttributedStringBuilder(attributedString: self).apply(with: attributes, for: string, to: position).build()
     }
 }
